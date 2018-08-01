d16c428c76f2d2dc1ad309ac6644a5becf0daf36
dustine32 <dustine32@gmail.com>
2018-07-31 17:19:07 -0700

    Updating evidence of paint_evidence earlier in pipeline

--- scripts/sql/paint_go_update/paint_evidence.sql
+++ scripts/sql/paint_go_update/paint_evidence.sql
@@ -17,6 +17,54 @@ where not exists (
   and pen.evidence_type_sid = 46 
   and pen.obsolescence_date is null;
 
+--Update evidence column to correct, non-obsolete GO annotation for PAINT EXP evidence records
+set search_path=panther_upl;
+update paint_evidence_new pen
+set pen.evidence = x.go_annotation_id
+from
+(select distinct ga.annotation_id go_annotation_id, pa.annotation_id paint_annotation_id, pe.evidence_id
+from
+(select parent_node_acc, unnest(string_to_array(child_leaf_node_acc, ',')) as leaf from node_all_leaves) pl, paint_annotation_new pa, node n, node n1, go_annotation_new ga, go_evidence_new ge, confidence_code cc, paint_evidence_new pe
+where pa.node_id = n.node_id
+and n.accession = pl.parent_node_acc
+and n1.node_id = ga.node_id
+and n1.accession = pl.leaf
+and pa.classification_id = ga.classification_id
+and pa.annotation_id = pe.annotation_id
+and pe.evidence_type_sid = 46
+and n.classification_version_sid = 24
+and n1.classification_version_sid = 24
+and ga.annotation_id = ge.annotation_id
+and ga.annotation_id != pe.annotation_id
+and ga.obsolescence_date is null
+and ge.confidence_code_sid = cc.confidence_code_sid
+and cc.confidence_code in ('EXP', 'IDA', 'IPI', 'IMP', 'IGI', 'IEP')) x
+where x.evidence_id = pen.evidence_id;
+
+--Update evidence column to correct, non-obsolete GO annotation for PAINT ANCESTOR evidence records
+set search_path=panther_upl;
+update paint_evidence_new pen
+set pen.evidence = x.ancestor_paint_annotation_id
+from
+(
+  select distinct pan.annotation_id child_paint_annotation_id, pan1.annotation_id ancestor_paint_annotation_id, pe.evidence_id
+  from
+  (
+    select child_node_acc, unnest(string_to_array(ancestor_node_acc, ',')) as ancestor from node_all_ancestors
+  ) ca, paint_annotation_new pan, node n, node n1, paint_annotation_new pan1, paint_evidence_new pe
+  where pan.node_id = n.node_id
+  and n.accession = ca.child_node_acc
+  and n1.node_id = pan1.node_id
+  and n1.accession = ca.ancestor
+  and pan.classification_id = pan1.classification_id
+  and pan1.obsolescence_date is null
+  and pan.annotation_id = pe.annotation_id
+  and pe.evidence_type_sid = 47
+  and n.classification_version_sid = 24
+  and n1.classification_version_sid = 24
+) x
+where pen.evidence_id = x.evidence_id;
+
 -- Go through the paint-annotation table, and see if this annotation_id is still exist in the paint_evidence table with obsoleted_by column is null, if not, obsolete the paint_annotation entry
 set search_path=panther_upl;
 update paint_annotation_new pa 
