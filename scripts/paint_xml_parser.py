685039b065acce256f278deda6090a2ab59d9273
dustine32 <dustine32@gmail.com>
2018-09-07 14:41:01 -0700

    Cleaning error result XML

--- scripts/paint_xml_parser.py
+++ scripts/paint_xml_parser.py
@@ -5,7 +5,22 @@ parser = ArgumentParser()
 parser.add_argument("filepath")
 args = parser.parse_args()
 
+### Clean up xml format
 filepath = args.filepath
+with open(filepath) as f:
+    raw_xml = f.read()
+
+xml_header = raw_xml.split("\n")[0]
+new_xml = raw_xml.replace(xml_header + "\n", "")
+root_tag = new_xml.split("\n")[0]
+if root_tag != "<searches>":
+    new_xml = "<searches>\n" + new_xml
+    new_xml = new_xml + "</searches>\n"
+new_xml = "<?xml version=\"1.0\"?>\n" + new_xml
+
+with open(filepath, "w") as wf:
+    wf.write(new_xml)
+
 # tree = ElementTree.parse("resources/SEARCH_TYPE_AGG_FAMILY_ANNOTATION_INFO.txt")
 tree = ElementTree.parse(filepath)
 root = tree.getroot()
