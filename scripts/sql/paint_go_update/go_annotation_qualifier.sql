4141d5704af99e35676e4c356d0432b3578362e0
dustine32 <dustine32@gmail.com>
2018-12-30 19:43:46 -0800

    go qualifier version variable

--- scripts/sql/paint_go_update/go_annotation_qualifier.sql
+++ scripts/sql/paint_go_update/go_annotation_qualifier.sql
@@ -6,6 +6,6 @@ INSERT INTO go_annotation_qualifier_new(annotation_qualifier_id, annotation_id, 
   select nextval('uids'), ga.annotation_id, q.qualifier_id 
   from go_annotation_new ga, go_classification_new gc, goanno_wf gw, gene g, gene_node gn, qualifier q 
   where ga.node_id = gn.node_id and gn.gene_id = g.gene_id and ga.obsolescence_date is null 
-  and g.classification_version_sid = 24 and gw.geneid = g.primary_ext_acc 
+  and g.classification_version_sid = {classification_version_sid} and gw.geneid = g.primary_ext_acc 
   and ga.classification_id = gc.classification_id and gc.accession = gw.go_acc 
   and q.qualifier = upper(gw.qualifier);
