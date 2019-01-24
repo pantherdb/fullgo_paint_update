b076f30fbe09f16084fbd361e91d0ba4c7523a22
dustine32 <dustine32@gmail.com>
2019-01-24 11:18:46 -0800

    Switch table names step for fullgo_version

--- scripts/sql/paint_go_update/switch_table_names.sql
+++ scripts/sql/paint_go_update/switch_table_names.sql
@@ -4,6 +4,8 @@ ALTER TABLE go_classification RENAME TO go_classification_old;
 ALTER TABLE go_classification_new RENAME TO go_classification;
 ALTER TABLE go_classification_relationship RENAME TO go_classification_relationship_old;
 ALTER TABLE go_classification_relationship_new RENAME TO go_classification_relationship;
+ALTER TABLE fullgo_version RENAME TO fullgo_version_old;
+ALTER TABLE fullgo_version_new RENAME TO fullgo_version;
 ALTER TABLE go_evidence RENAME TO go_evidence_old;
 ALTER TABLE go_evidence_new RENAME TO go_evidence;
 ALTER TABLE go_annotation RENAME TO go_annotation_old;
