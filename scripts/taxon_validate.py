fcfad3feaadbfc23c32a1cd277fd92826422892b
dustine32 <dustine32@gmail.com>
2019-04-16 11:00:51 -0700

    Class-ifying taxon_validate script

--- scripts/taxon_validate.py
+++ scripts/taxon_validate.py
@@ -1,16 +1,17 @@
 from Bio import Phylo
 import csv
 import argparse
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel("DEBUG")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-o", "--output_file")
 parser.add_argument("-s", "--slim_terms")
 parser.add_argument("-t", "--taxon_term_table")
 parser.add_argument("-p", "--panther_tree_nhx")
-args = parser.parse_args()
-
-# taxon_term_file = "taxon_term_table_converted"
-taxon_term_file = "TaxonConstraintsLookup.txt"
+# args = parser.parse_args()
 
 MADEUP_SPECIES = [
     "Saccharomycetaceae-Candida",
@@ -30,34 +31,6 @@ MADEUP_SPECIES = [
     "Artiodactyla",
     "LUCA"
 ]
-
-slim_terms = []
-if args.slim_terms:
-    # Get list of slim terms to filter for
-    slim_file = open(args.slim_terms)
-    for t in slim_file.readlines():
-        slim_terms.append(t.rstrip())
-    slim_file.close()
-
-taxon_indexes = {}
-term_constraint_lists = {}
-# with open(taxon_term_file) as t3f:
-with open(args.taxon_term_table) as t3f:
-    header = t3f.readline().rstrip()
-    headers = header.split("\t")
-    index_count = 0
-    for h in headers[1:len(headers)]:
-        taxon_indexes[h] = index_count
-        index_count += 1
-
-    for l in t3f.readlines():
-        cols = l.split("\t")
-        go_term = cols[0]
-        if len(slim_terms) == 0 or go_term in slim_terms:
-            term_constraint_lists[go_term] = cols[1:len(cols)]
-
-print("taxon_indexes: {}".format(len(taxon_indexes)))
-print("term_constraint_lists: {}".format(len(term_constraint_lists)))
 
 def extract_clade_name(clade_comment):
     if clade_comment is None:
@@ -85,58 +58,88 @@ def find_taxon_clade(taxon_name, root_clade):
             if result is not None:
                 return result
 
-# Parse species_tree
-# tree = next(Phylo.parse("resources/species_pthr13_annot.nhx","newick"))
-tree = next(Phylo.parse(args.panther_tree_nhx,"newick"))
-tree.clade.name = extract_clade_name(tree.clade.comment)
-name_children(tree.clade)
 
-def validate_taxon_term(taxon, term):
-    # node_path[-2] won't work for LUCA. LUCA should equal NCBITaxon:131567 for "cellular organisms".
-    # Need to rerun gaferencer to include this taxon, then convert "cellular organisms" header to "LUCA" in
-    # taxon_to_oscode.py
-    # print(taxon)
-    while taxon in MADEUP_SPECIES and taxon != "LUCA":
+class TaxonTermValidator:
+    def __init__(self, taxon_term_table, panther_tree_nhx, slim_terms=None):
+        self.term_constraint_lists = {}
+        self.taxon_indexes = {}
+        self.slim_terms = []
+        self.tree = None
+
+        if slim_terms:
+            # Get list of slim terms to filter for
+            slim_file = open(slim_terms)
+            for t in slim_file.readlines():
+                self.slim_terms.append(t.rstrip())
+            slim_file.close()
+        
+        with open(taxon_term_table) as t3f:
+            header = t3f.readline().rstrip()
+            headers = header.split("\t")
+            index_count = 0
+            for h in headers[1:len(headers)]:
+                self.taxon_indexes[h] = index_count
+                index_count += 1
+
+            for l in t3f.readlines():
+                cols = l.split("\t")
+                go_term = cols[0]
+                if len(self.slim_terms) == 0 or go_term in self.slim_terms:
+                    self.term_constraint_lists[go_term] = cols[1:len(cols)]
+
+        logger.debug("taxon_indexes: {}".format(len(self.taxon_indexes)))
+        logger.debug("term_constraint_lists: {}".format(len(self.term_constraint_lists)))
+
+        # Parse species_tree
+        self.tree = next(Phylo.parse(panther_tree_nhx, "newick"))
+        self.tree.clade.name = extract_clade_name(self.tree.clade.comment)
+        name_children(self.tree.clade)
+
+    def validate_taxon_term(self, taxon, term):
+        # node_path[-2] won't work for LUCA. LUCA should equal NCBITaxon:131567 for "cellular organisms".
+        # Need to rerun gaferencer to include this taxon, then convert "cellular organisms" header to "LUCA" in
+        # taxon_to_oscode.py
         # print(taxon)
-        # Get parent of taxon - handy BioPython trick
-        taxon_clade = find_taxon_clade(taxon, tree.clade)
-        node_path = tree.get_path(taxon_clade)
-        if len(node_path) > 1:
-            parent_clade = node_path[-2]
-        else:
-            parent_clade = tree.clade
-        taxon = parent_clade.name
+        while taxon in MADEUP_SPECIES and taxon != "LUCA":
+            # print(taxon)
+            # Get parent of taxon - handy BioPython trick
+            taxon_clade = find_taxon_clade(taxon, self.tree.clade)
+            node_path = self.tree.get_path(taxon_clade)
+            if len(node_path) > 1:
+                parent_clade = node_path[-2]
+            else:
+                parent_clade = self.tree.clade
+            taxon = parent_clade.name
 
-    # Remove after getting LUCA's real values - assuming most everything is cool with LUCA
-    if taxon == "LUCA":
-        # virus stuff
-        if term in ("GO:0019012", "GO:0039679", "GO:0044423"):
+        # Remove after getting LUCA's real values - assuming most everything is cool with LUCA
+        if taxon == "LUCA":
+            # virus stuff
+            if term in ("GO:0019012", "GO:0039679", "GO:0044423"):
+                return False
+            else:
+                return True
+
+        try:
+            result = self.term_constraint_lists[term][self.taxon_indexes[taxon]]
+        except IndexError:
+            print(taxon)
+            result = self.term_constraint_lists[term][self.taxon_indexes[taxon]]
+        if result == '0':
             return False
-        else:
-            return True
+        return True
 
-    try:
-        result = term_constraint_lists[term][taxon_indexes[taxon]]
-    except IndexError:
-        print(taxon)
-        result = term_constraint_lists[term][taxon_indexes[taxon]]
-    if result == '0':
-        return False
-    return True
-
-def append_madeup_species_to_table():
+def append_madeup_species_to_table(validator : TaxonTermValidator, output_file):
     # List hyphenated species
     # Reconstruct entire table file
     # with open("new_table_file", "w+") as nf:
-    with open(args.output_file, "w+") as nf:
+    with open(output_file, "w+") as nf:
         writer = csv.writer(nf, delimiter="\t")
         header = ["GOterm"]
-        out_rows = []
         out_term_values = {}
-        for tk in list(taxon_indexes.keys()) + MADEUP_SPECIES:
+        for tk in list(validator.taxon_indexes.keys()) + MADEUP_SPECIES:
             header.append(tk)
-            for term in term_constraint_lists:
-                if validate_taxon_term(tk, term):
+            for term in validator.term_constraint_lists:
+                if validator.validate_taxon_term(tk, term):
                     result = 1
                 else:
                     result = 0
@@ -147,8 +150,11 @@ def append_madeup_species_to_table():
 
         writer.writerow(header)
         for otv in out_term_values:
-            if len(slim_terms) == 0 or otv in slim_terms:
+            if len(validator.slim_terms) == 0 or otv in validator.slim_terms:
                 writer.writerow(out_term_values[otv])
 
 if __name__ == "__main__":
-    append_madeup_species_to_table()
+    args = parser.parse_args()
+
+    validator = TaxonTermValidator(args.taxon_term_table, args.panther_tree_nhx)
+    append_madeup_species_to_table(validator, args.output_file)
