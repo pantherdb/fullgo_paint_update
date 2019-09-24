c54deafd0ac2ba3dc2eaf2d9f57bb2cd00e04120
dustine32 <dustine32@gmail.com>
2019-09-24 09:54:07 -0700

    SQL for loading go_evidence qualifiers

--- scripts/sql/paint_go_update/go_annotation_qualifier.sql
+++ scripts/sql/paint_go_update/go_annotation_qualifier.sql
@@ -9,3 +9,29 @@ INSERT INTO go_annotation_qualifier_new(annotation_qualifier_id, annotation_id, 
   and g.classification_version_sid = {classification_version_sid} and gw.geneid = g.primary_ext_acc 
   and ga.classification_id = gc.classification_id and gc.accession = gw.go_acc 
   and q.qualifier = upper(gw.qualifier);
+
+-- Fill annotation_qualifier_id column on go_evidence
+-- Use goanno_wf to associate to go_evidence
+set search_path = panther_upl;
+update go_evidence_new ge_u
+set annotation_qualifier_id = gaq.annotation_qualifier_id
+from go_evidence_new ge
+join go_annotation_new ga on ga.annotation_id = ge.annotation_id
+join gene_node gn on gn.node_id = ga.node_id
+join gene g on g.gene_id = gn.gene_id
+join go_classification_new gc on gc.classification_id = ga.classification_id
+join confidence_code cc on cc.confidence_code_sid = ge.confidence_code_sid
+join evidence_type et on et.evidence_type_sid = ge.evidence_type_sid
+join go_annotation_qualifier_new gaq on gaq.annotation_id = ga.annotation_id
+join qualifier q on q.qualifier_id = gaq.qualifier_id
+join (select geneid, go_acc, confidence_code, split_part(unnest(string_to_array(evidence, '|')), ':', 1) as evidence_type, split_part(unnest(string_to_array(evidence, '|')), ':', 2) as evidence, qualifier 
+      from goanno_wf
+      where qualifier is not null) gw
+      on gw.geneid = g.primary_ext_acc
+      and gw.go_acc = gc.accession
+      and gw.confidence_code = cc.confidence_code
+      and gw.evidence_type = et.type
+      and gw.evidence = ge.evidence
+      and upper(gw.qualifier) = q.qualifier
+where g.classification_version_sid = {classification_version_sid}
+and ge_u.evidence_id = ge.evidence_id;
