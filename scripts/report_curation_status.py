6d5606b5cafd9d05dfd0a70d6399cc7ea4761087
dustine32 <dustine32@gmail.com>
2020-07-14 08:22:00 -0700

    Use version variable in config for issue #42

--- scripts/report_curation_status.py
+++ scripts/report_curation_status.py
@@ -10,13 +10,13 @@ select c.accession, cst.status, u.name, cs.creation_date from panther_upl.curati
 join panther_upl.classification c on c.classification_id = cs.classification_id
 join panther_upl.users u on u.user_id = cs.user_id
 join panther_upl.curation_status_type cst on cst.status_type_sid = cs.status_type_sid
-where c.classification_version_sid = 26
---and cs.creation_date > '{}'
+where c.classification_version_sid = {classification_version_sid}
 order by cs.creation_date desc;
 """
 
 start_date = "2019-06-01"
-results = CALLER.run_cmd_line_args(query.format(start_date), no_header_footer=True)
+# results = CALLER.run_cmd_line_args(query.format(start_date), no_header_footer=True)
+results = CALLER.run_cmd_line_args(query, no_header_footer=True)
 
 date_str = datetime.date.today().isoformat()
 sheet_title = "{}_curation_status".format(date_str)
